e80dd384a009e3eb76819552f9956482febb0f7d
Amanda Shafack <shafack.likhene@gmail.com>
2021-08-03 20:55:09 +0100

    Add a gitignore file

---
+++ time_tracker_flutter_course/lib/app/home/jobs/jobs_page.dart
@@ -0,0 +1,103 @@
+// @dart=2.9
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/common_widgets/show_exception_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
+
+import 'models/job.dart';
+
+class JobsPage extends StatelessWidget {
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final auth = Provider.of<AuthBase>(
+        context,
+        listen: false,
+      );
+      await auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final didRequestSignOut = await showAlertDialog(
+      context,
+      title: 'Logout',
+      content: 'Are you sure that you want to logout',
+      cancelActionText: 'Cancel',
+      defaultActionText: 'Logout',
+    );
+    if (didRequestSignOut == true) {
+      _signOut(context);
+    }
+  }
+
+  // Writes data to Firestore
+  void _createJob(BuildContext context) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.createJob(Job(
+        name: 'Blogging',
+        ratePerHour: 10,
+      ));
+    } on FirebaseException catch (e) {
+      showExceptionAlertDialog(
+        context,
+        title: 'Operation failed',
+        exception: e,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jobs Page'),
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              'Logout',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () => _confirmSignOut(context),
+          ),
+        ],
+      ),
+      body: _buildContents(context),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _createJob(context),
+      ),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder<List<Job>>(
+        stream: database.jobsStream(),
+        builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final jobs = snapshot.data;
+              final children = jobs.map((job) => Text(job.name)).toList();
+              return ListView(
+                children: children,
+              );
+            }
+          if (snapshot.hasError)
+            {
+              return Center(child: Text('An error has occurred'?? "We can't help you ya"),);
+            }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+}
