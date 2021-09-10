036aea19973530f743f75f69ab20f513b81f87c2
Amanda Shafack <shafack.likhene@gmail.com>
2021-09-10 21:10:24 +0100

    Complete the Bitcoin Ticker challenge

---
+++ bitcoin_ticker_flutter/lib/coin_data.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'API-KEY-HERE';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      Uri url =
+          Uri.parse('$coinAPIURL/$crypto/$selectedCurrency?apiKey=$apiKey');
+      http.Response response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
